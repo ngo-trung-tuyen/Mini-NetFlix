6574605d37ede18a3405f5867fb9ea7616ce6574
Ngô Trung Tuyến <tuyenngo@tuyenngoo.local>
2025-05-24 12:17:57 +0700

    feat: application-logging

--- src/main/java/com/mininetflix/MiniNetflixApplication.java
+++ src/main/java/com/mininetflix/MiniNetflixApplication.java
@@ -1,12 +1,14 @@
 package com.mininetflix;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class MiniNetflixApplication {
-
     public static void main(String[] args) {
+        log.info("Starting MiniNetflixApplication");
         SpringApplication.run(MiniNetflixApplication.class, args);
     }
 
